120ec6a6ade662758d39fc529f769856a49cd68d
Artyom <akuzikov@mail.ru>
2020-06-30 14:41:04 +0300

    transfered TodoApp

--- source/app/index.js
+++ source/app/index.js
@@ -1,43 +1,7 @@
 import '../components/base.scss'
 import '../components/TodoApp';
 
-import ItemTemplate from '@components/TodoApp/itemTemplate';
-import { GenerateNum } from '@components/GenerateNum';
-
-const itemTemplate = new ItemTemplate();
-const getNewUserID = new GenerateNum();
-
-class TodoApp {
-    constructor() {
-        this.todoListArray = [];
-        this.newId = getNewUserID.generateByDate();
-        this.todoList = document.querySelector('.todo-app__list');
-    }
-    
-    getNewId() {
-        return this.newId;
-    }
-    
-    clearInput() {
-        document.querySelector('.todo-app__input').value = '';
-    }
-    
-    addToArray(text) {
-        this.todoListArray.push({
-            id: this.getNewId(),
-            title: text,
-            completed: false
-        });
-        
-        this.insertTodoText();
-        this.clearInput();
-        console.log(this.todoListArray);
-    }
-    
-    insertTodoText() {
-        this.todoList.innerHTML = itemTemplate.listItem(this.todoListArray);
-    }
-}
+import TodoApp from '@components/TodoApp/TodoApp';
 
 const todoApp = new TodoApp();
 
---
+++ source/components/TodoApp/TodoApp.js
@@ -0,0 +1,37 @@
+import ItemTemplate from './itemTemplate';
+import { GenerateNum } from '../GenerateNum';
+
+const itemTemplate = new ItemTemplate();
+const getNewUserID = new GenerateNum();
+
+export default class TodoApp {
+    constructor() {
+        this.todoListArray = [];
+        this.newId = getNewUserID.generateByDate();
+        this.todoList = document.querySelector('.todo-app__list');
+    }
+    
+    getNewId() {
+        return this.newId;
+    }
+    
+    clearInput() {
+        document.querySelector('.todo-app__input').value = '';
+    }
+    
+    addToArray(text) {
+        this.todoListArray.push({
+            id: this.getNewId(),
+            title: text,
+            completed: false
+        });
+        
+        this.insertTodoText();
+        this.clearInput();
+        console.log(this.todoListArray);
+    }
+    
+    insertTodoText() {
+        this.todoList.innerHTML = itemTemplate.listItem(this.todoListArray);
+    }
+}
